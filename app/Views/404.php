e1742342f4fab67713f5ee903ad5fcc6f0056797
priss0u <mandozipriscille@gmail.com>
2025-09-15 15:31:31 +0200

    fin

---
+++ app/Views/404.php
@@ -0,0 +1,8 @@
+<?php 
+require_once(__DIR__ . '/partials/head.php');
+?>
+<h1 class="text-danger">Erreur</h1>
+
+<?php 
+require_once(__DIR__ . '/partials/footer.php');
+?>
---
+++ app/Views/home.view.php
@@ -0,0 +1,8 @@
+<?php 
+require_once(__DIR__ . '/partials/head.php');
+?>
+<h1>Bienvenue ! </h1>
+
+<?php 
+require_once(__DIR__ . '/partials/footer.php');
+?>
