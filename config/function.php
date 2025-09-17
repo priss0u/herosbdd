1ebc0347caf0f588ee5acd80537c07b399682deb
priss0u <mandozipriscille@gmail.com>
2025-09-17 16:20:33 +0200

    connecter un user session

--- config/function.php
+++ config/function.php
@@ -1,6 +1,5 @@
 <?php 
     function redirectToRoute($route){
-        http_response_code(308);
         header("Location: {$route}");
         exit;
     }
@@ -9,6 +8,17 @@
 
         ?>
             <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?= $myMessage ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php
+
+    }
+
+    function errorMessage($myMessage){
+
+        ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <?= $myMessage ?>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
