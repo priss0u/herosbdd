cb72b62468ef11f1b37e66ca2682b21380ea5012
priss0u <mandozipriscille@gmail.com>
2025-09-17 13:58:37 +0200

    connecter un user et adresse mail non double et session

--- config/router.php
+++ config/router.php
@@ -1,12 +1,13 @@
 <?php 
-require_once (__DIR__ . "/database.php");
+    require_once (__DIR__ . "/function.php");
+    require_once (__DIR__ . "/database.php");
 
 // Définition des routes
 // Ce tableau associe des chemins d'URI à des fichiers de contrôleurs spécifiques
 // Le chemin dans l'URL (comme '/') est relié au contrôleur correspondant (comme 'HomeController.php')
 $routes = [
     '/' => 'HomeController.php',
-    '/register' => 'RegisterController.php',
+    '/register' => 'RegisterController.php'
 ];
 
 // Récupération de l'URI actuelle de la requête utilisateur
@@ -17,7 +18,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 // Vérification de l'existence de la route dans le tableau des routes
 // Si l'URI demandée existe dans le tableau, le contrôleur associé est inclus
 if(array_key_exists($uri, $routes)){
-;
+
     // Inclusion dynamique du fichier contrôleur correspondant à l'URI
     require_once(__DIR__ . "/../app/Controllers/" . $routes[$uri]);
 
